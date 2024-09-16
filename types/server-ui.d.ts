bf53c1f4c97a31b1fa3730518b9916ffae266777
iEnis <enis-neimi@outlook.de>
2024-09-16 18:34:18 +0200

    changed the top info text to be blue/?

--- types/server-ui.d.ts
+++ types/server-ui.d.ts
@@ -1,4 +1,4 @@
-//! This file has been patched "https://www.npmjs.com/package/typesafe-mc"
+//? This file has been patched "https://www.npmjs.com/package/typesafe-mc"
 //? version: "{PATCH_VERSION}"
 
 // Type definitions for Minecraft Bedrock Edition script APIs
